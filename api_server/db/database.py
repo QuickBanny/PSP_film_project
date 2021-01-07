de4788ffbb0fabc0b6af226462af49546571370d
quickbanny <maiorovpavel@gmail.com>
2021-01-07 17:33:39 +0600

    add methods in DBSession

--- api_server/db/database.py
+++ api_server/db/database.py
@@ -1,4 +1,5 @@
 from sqlalchemy.engine import Engine
+from sqlalchemy.exc import IntegrityError, DataError
 from sqlalchemy.orm import sessionmaker, Session
 
 
@@ -7,6 +8,32 @@ class DBSession:
 
     def __init__(self, session: Session):
         self._session = session
+
+    def query(self, *args, **kwargs):
+        return self._session.query(*args, **kwargs)
+
+    def close_session(self):
+        self._session.close()
+
+    def add_model(self, model: BaseModel):
+        try:
+            self._session.add(model)
+        except IntegrityError as e:
+            pass
+        except DataError as e:
+            pass
+
+    def commit_session(self, need_close: bool = False):
+        try:
+            self._session.connection()
+        except IntegrityError as e:
+            pass
+        except DataError as e:
+            pass
+
+        if need_close:
+            self.close_session()
+
 
 
 class DataBase:
